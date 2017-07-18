4d9c65b2742e269c52ce60383a31fe13e64bc5c2
roman.mishchenko@gmail.com <roman.mishchenko@gmail.com>
2017-07-18 22:30:57 +0300

    first commit

---
+++ src/main/java/com/algoritmed/j2c/amj2c_1/AmJ2c1Application.java
@@ -0,0 +1,12 @@
+package com.algoritmed.j2c.amj2c_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmJ2c1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmJ2c1Application.class, args);
+	}
+}
---
+++ src/test/java/com/algoritmed/j2c/amj2c_1/AmJ2c1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.algoritmed.j2c.amj2c_1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AmJ2c1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
