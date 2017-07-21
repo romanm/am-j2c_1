d1aaddf8dc9cc2a895a614d073866423c206429b
roman.mishchenko@gmail.com <roman.mishchenko@gmail.com>
2017-07-22 02:30:31 +0300

    autoSave stub

--- src/main/java/com/algoritmed/j2c/amj2c_1/DbRest.java
+++ src/main/java/com/algoritmed/j2c/amj2c_1/DbRest.java
@@ -12,11 +12,23 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DbRest {
 	private static final Logger logger = LoggerFactory.getLogger(DbRest.class);
+
+	@PostMapping("/r/table/save")
+	public @ResponseBody Map<String, Object> tableSave(
+			@RequestBody Map<String, Object> dbSaveObj
+			) {
+		logger.info("\n "
+				+ "/r/table/save");
+		System.err.println(dbSaveObj);
+		return dbSaveObj;
+	}
 
 	private @Value("${sql.join_columns.select}") String sqlJoinColumnsSelect;
 	private @Value("${sql.table.select}") String sqlTableSelect;
--- src/main/resources/static/f/js/am-j2C_1.js
+++ src/main/resources/static/f/js/am-j2C_1.js
@@ -1,7 +1,79 @@
+function initI2c($scope, $http, $timeout){
+	$scope.j2C = {};
+	$scope.j2C.treeObjConfig= {
+		'Константи':{}
+		,'Довідники':{}
+		,'Документи':{
+			'Общіе реквізіти':{}
+			,'Нумератори':{}
+			,'Последовательность':{}
+		}
+		,'Журнали документів':{}
+		,'Перечислення':{}
+		,'Звіти':{}
+		,'Обробки':{}
+		,'Плани рахунків':{}
+		,'Види субконто':{}
+		,'Операції':{}
+	};
+	$scope.$watch("j2C.tableSelect.historyEditTable.count", function handleChange( newValue, oldValue ) {
+		console.log(newValue);
+		console.log($scope.j2C.tableSelect.historyEditTable);
+		$scope.j2C.tableSelect.historyEditTable.fn_checkToSave();
+	});
+	$scope.j2C.tableSelect= {
+		historyEditTable:{
+			count:0
+			,data:{}
+			,maxChange:10
+			,save_count:0
+			,autoSaveTextTypingPause:2000
+			,add:function(){this.count++;}
+			,fn_timeout_checkToSave:null
+			,fn_checkToSave:function(){
+				if(this.count>this.maxChange){
+					this.fn_httpSave();
+					this.count=0;
+					this.save_count++;
+				}
+				$timeout.cancel(this.fn_timeout_checkToSave);
+				var thisObj = this;
+				this.fn_timeout_checkToSave = $timeout(function(){
+					if(thisObj.count!=0){
+						thisObj.fn_httpSave();
+						thisObj.count=0;
+						thisObj.save_count++;
+					}
+				}, this.autoSaveTextTypingPause);
+			}
+			,fn_httpSave:function(){
+				$http.post('/r/table/save', this.data).then(function(response) {
+					console.log('/r/table/save');
+					console.log(response.data);
+				});
+			}
+		}
+		,td:{
+			isInEdit:function(r,k){
+				var isInEdit = !!$scope.j2C.tableSelect.historyEditTable.data[r.row_id] 
+					&& !!$scope.j2C.tableSelect.historyEditTable.data[r.row_id][k];
+				return isInEdit;
+			}
+			,click:function(r,k){
+				if($scope.j2C.tableSelect.historyEditTable){
+					if(!$scope.j2C.tableSelect.historyEditTable.data[r.row_id])
+						$scope.j2C.tableSelect.historyEditTable.data[r.row_id]={};
+					$scope.j2C.tableSelect.historyEditTable.data[r.row_id][k]=r['col_'+k];
+					$scope.j2C.tableSelect.historyEditTable.add();
+				}
+			}
+		}
+	}
+	
+}
 var app = angular.module('j2CApp', []);
-app.controller('J2CCtrl', function($scope, $http) {
-	$scope.firstName= "John";
-	$scope.lastName= "Doe";
+app.controller('J2CCtrl', function($scope, $http, $timeout) {
+	initI2c($scope, $http, $timeout);
 	$scope.tableTable= {
 		tablename:'Таблиця'
 		,fieldname:'Колонка'
@@ -29,4 +101,6 @@ app.controller('J2CCtrl', function($scope, $http) {
 		console.log($scope.col_alias);
 	});
 
+	
+	
 });
