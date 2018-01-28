953e3fddb1923da7e6f5253d1adfe0c04a809614
romanm <roman.mishchenko@gmail.com>
2018-01-28 23:34:08 +0200

    algoritmedJs "hello world" commit 2

--- src/main/resources/static/f/angular-edu/compile-dynamic-HTML.js
+++ src/main/resources/static/f/angular-edu/compile-dynamic-HTML.js
@@ -1,6 +1,6 @@
 var app = angular.module('app', []);
 
-app.directive('amdRun', function ($compile) {
+app.directive('amdRun', function ($compile, $http) {
 //	restrict: 'A',
 //	replace: true,
 	return {
@@ -8,34 +8,63 @@ app.directive('amdRun', function ($compile) {
 		scope: true,
 		link: function (scope, ele, attrs) {
 			scope.$watch(attrs['amdRun'], function(program_init) {
-				console.log(scope)
 				console.log(ele)
-				console.log(ele.html)
-				if(!scope.algoritmed.inits[program_init.programName]){
-					scope.algoritmed.inits[program_init.programName]
+				console.log(ele[0].id)
+				console.log(program_init)
+				if(program_init.programPath){
+					$http.get(program_init.programPath).then(function(response) {//read a program
+						var amdRun = response.data;
+						console.log(amdRun)
+						angular.forEach(amdRun, function(v, k){
+							console.log(k)
+							console.log(v)
+							var amdRun_pr_uri = '/f/algoritmed/lib/'+k+'.html'
+							console.log(amdRun_pr_uri)
+							$http.get(amdRun_pr_uri).then(function(response) {//read a program
+								var pr = response.data;
+								console.log(pr)
+								ele.html(pr);
+								$compile(ele.contents())(scope);
+							})
+						});
+					});
+				}else
+				if(program_init.programName){
+					if(!scope.algoritmed.inits[program_init.programName]){
+						scope.algoritmed.inits[program_init.programName]
 						= program_init;
-					console.log(scope.algoritmed)
+						console.log(scope.algoritmed)
+					}
 				}
-				ele.html(program_init.html);
-				$compile(ele.contents())(scope);
+				if(program_init.html){
+					ele.html(program_init.html);
+					$compile(ele.contents())(scope);
+				}
 			});
 		}
 	};
 });
 
-app.controller('Controller', ['$scope', function($scope) {
+app.controller('Controller', function($scope, $http) {
     $scope.format = 'M/d/yy h:mm:ss a';
+    $scope.amdRunObj = function() {
+    	console.log(this)
+    }
     $scope.click = function(arg) {
-    	alert('Clicked ' + arg);
+//    	alert('Clicked ' + arg);
+    	console.log($scope.algoritmed);
     }
     $scope.algoritmed = {
     	programs:{}
         ,htmls:{}
     	,dbs:{}
     	,inits:{}
+    	,amdRuns:[
+    		{programPath:'/f/algoritmed/sample001/helloworld.json'}
+    	]
     }
     $scope.text='Hello World!'
     $scope.programs = {}
     $scope.programs.html = '<a ng-click="click(1)" href="#">Click me {{text}} </a>';
 //    $scope.html = '<a ng-click="click(1)" href="#">Click me</a>';
-  }]);
+  });
