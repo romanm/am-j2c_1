9d213c5d991e2f81c6dad5b1bc4d5068f6396cd5
romanm <roman.mishchenko@gmail.com>
2018-02-15 07:20:15 +0200

    code clean

--- src/main/java/com/algoritmed/j2c/amj2c_1/filters/SessionFilter.java
+++ src/main/java/com/algoritmed/j2c/amj2c_1/filters/SessionFilter.java
@@ -1,15 +1,14 @@
-package com.algoritmed.j2c.amj2c_1;
+package com.algoritmed.j2c.amj2c_1.filters;
 
 import java.io.IOException;
-import java.util.Locale;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Component;
 
@@ -17,25 +16,19 @@ import org.springframework.stereotype.Component;
 public class SessionFilter implements Filter{
 
 	@Override
-	public void destroy() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
         String url = ((HttpServletRequest)request).getRequestURL().toString();
         String queryString = ((HttpServletRequest)request).getQueryString();
         System.err.println("--30-- "+url + (null==queryString?"":"?"+queryString));
-        
 		chain.doFilter(request, response);
 	}
 
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {
-		// TODO Auto-generated method stub
-		
+	}
+	@Override
+	public void destroy() {
 	}
 
 }
