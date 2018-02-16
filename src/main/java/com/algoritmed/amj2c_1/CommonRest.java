f1fd24a96599e438f1050266460bbd526ae81719
romanm <roman.mishchenko@gmail.com>
2018-02-16 04:15:28 +0200

    code clean

--- src/main/java/com/algoritmed/amj2c_1/CommonRest.java
+++ src/main/java/com/algoritmed/amj2c_1/CommonRest.java
@@ -6,14 +6,14 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.algoritmed.amj2c_1.components.ReadJsonFromFile;
 
-@RestController
+@Controller
 public class CommonRest {
 	private static final Logger logger = LoggerFactory.getLogger(CommonRest.class);
 	@GetMapping("/v/{page1}")
